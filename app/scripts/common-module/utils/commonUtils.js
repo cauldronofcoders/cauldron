ff9cf660da1e577ba3ab86988e8ab0a3d15c2665
Mangesh Wanjari <mangesh.wanjari@accenture.com>
2016-01-14 23:32:04 +0530

    Updated doc and delete unused JSON files.

--- app/scripts/common-module/utils/commonUtils.js
+++ app/scripts/common-module/utils/commonUtils.js
@@ -1,5 +1,5 @@
 /**
- * @description creating a module extending TestApp
+ * @description creating a module extending myMPCSApp
  */
 angular.module('myMPCSApp.commonUtil', [])
     .service('commonUtils', function() {
