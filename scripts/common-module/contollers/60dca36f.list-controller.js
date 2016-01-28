6a8fc4accd374aa286376335ffb9e4b3a6d15c14
Deepak Dhamuria <deepak.k.dhamuria@accenture.com>
2016-01-28 12:53:46 +0000

    Updates

--- scripts/common-module/contollers/60dca36f.list-controller.js
+++ scripts/common-module/contollers/60dca36f.list-controller.js
@@ -1,7 +1,7 @@
 
 angular.module('myMPCSApp.listController',[])
 .controller('listController', function($scope, URL_CONST, facadeApiFactory) {
-
+$scope.shoppingCart = [];
    /*@description function to get the list of phone devices */
   $scope.getDeviceData = function() {
     facadeApiFactory.getRequest(URL_CONST.BASE_URL_LIST,
@@ -12,4 +12,19 @@ angular.module('myMPCSApp.listController',[])
     });
   };
    $scope.getDeviceData();
+
+    $scope.addToCart = function (device) {
+			var found = false;
+			$scope.shoppingCart.forEach(function (item) {
+				if (item.deviceName === device.deviceName) {
+                   item.quantity++;
+					found = true;
+				}
+			});
+			if (!found) {
+               $scope.shoppingCart.push(angular.extend({quantity: 1}, device));
+
+			}
+		};
+
 });
