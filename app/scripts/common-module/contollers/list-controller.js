352f631e28f8be13e372f11d0dc7990ea07b309c
Chanchal Narang <chanchal.narang@t-mobile.com>
2016-02-03 10:32:47 +0530

    quantity manual change and update through dropdown on view cart page

--- app/scripts/common-module/contollers/list-controller.js
+++ app/scripts/common-module/contollers/list-controller.js
@@ -2,7 +2,8 @@
 angular.module('myMPCSApp.listController',[])
 .controller('listController', function($scope,$rootScope, URL_CONST, facadeApiFactory,SessionService) {
 $scope.shoppingCart = [];
-
+$rootScope.quantity_selected=1;
+    alert("value after changing as text"+$scope.quantity_selected);
    /*@description function to get the list of phone devices */
   $scope.getDeviceData = function() {
     facadeApiFactory.getRequest(URL_CONST.BASE_URL_LIST,
@@ -18,17 +19,18 @@ $scope.shoppingCart = [];
    /*@description function to add phone device to cart */
 $scope.addToCart = function(id,value,quantity) {
     $scope.found = false;
-
-    $scope.shopCart = {
+ $scope.shopCart = {
         deviceDetail : value,
-        quant : quantity
+        quant : $scope.quantity_selected
     } ;
 
+console.log("quant"+$scope.shopCart.quant);
           for(var item in $scope.shoppingCart){
         if($scope.shoppingCart[item]==id){
-            $scope.shopCart.quant = $scope.shopCart.quant+quantity;
+            $scope.shopCart.quant = $scope.quantity_selected+1;
             SessionService.save(id,JSON.stringify($scope.shopCart));
              $scope.found = true;
+            console.log("same product"+$scope.shopCart.quant);
         }
 
         }
@@ -36,11 +38,28 @@ $scope.addToCart = function(id,value,quantity) {
 
         // new item, add now
         if (!$scope.found) {
+           $scope.quantity_selected=1;
+            $scope.shopCart.quant= $scope.quantity_selected;
             SessionService.save(id,JSON.stringify($scope.shopCart));
             $scope.shoppingCart.push(id);
+
+            console.log("new  product added"+$scope.shopCart.quant)
         }
              }
 
+
+$scope.updateQuantity=function(id,change,value){
+
+
+    $scope.shopCart_update = {
+        deviceDetail : value,
+        quant : change
+    } ;
+  alert("id is :::"+id);
+ SessionService.save(id,JSON.stringify( $scope.shopCart_update));
+   alert("inside function,quantt :::"+$scope.shopCart_update.quant);
+
+}
 
    /*@description function to get the list of phone devices added in cart */
   $scope.getItem = function() {
